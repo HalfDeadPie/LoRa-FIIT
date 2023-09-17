6bff1b8460076da65861cd2d79d0ac4690bc88c4
Alexander Valach <alexande.valachr@gmail.com>
2023-09-17 22:17:14 +0200

    Implemented FREQ switching for TS

--- src/ThompsonSampling.cpp
+++ src/ThompsonSampling.cpp
@@ -11,6 +11,14 @@ ThompsonSampling::ThompsonSampling()
     pulls[idx] = 0;
     estimatedMeans[idx] = TS_SCALE / 2;  // Initial scaled estimated mean for each SF
   }
+
+  #if FREQ_ENABLE
+    for (uint8_t idx = 0; idx < NUM_FREQ; idx++) {
+      freqRewards[idx] = 0;
+      freqPulls[idx] = 0;
+      freqEstimatedMeans[idx] = TS_SCALE / 2;  // Initial scaled estimated mean for each FREQ
+    }
+  #endif
 }
 
 ThompsonSampling::~ThompsonSampling() 
@@ -22,8 +30,8 @@ ThompsonSampling::~ThompsonSampling()
  * 
  * @return uint8_t 
  */
-uint8_t ThompsonSampling::pull() {
-  uint8_t bestSF = MIN_SF;
+uint8_t ThompsonSampling::pull(uint8_t currentSF) {
+  uint8_t bestSF = currentSF;
   uint16_t maxMean = estimatedMeans[0];
 
   for (uint8_t sf = MIN_SF; sf <= MAX_SF; sf++) {
@@ -44,14 +52,9 @@ uint8_t ThompsonSampling::pull() {
  * @param sf 
  * @param reward 
  */
-void ThompsonSampling::update(uint8_t sf, uint8_t reward)
+void ThompsonSampling::update(uint8_t currentSF, uint8_t reward)
 {
-  uint8_t idx = sf - MIN_SF;
-  
-  // Check againts index overflows
-  if (idx < MIN_SF && idx > MAX_SF) {
-    return;
-  } 
+  uint8_t idx = currentSF - MIN_SF;
   
   pulls[idx]++;
   rewards[idx] += reward * TS_SCALE;
@@ -61,3 +64,46 @@ void ThompsonSampling::update(uint8_t sf, uint8_t reward)
   }
 }
 
+#if FREQ_ENABLE
+  uint8_t ThompsonSampling::freqPull(uint8_t currentFREQ) 
+  {
+    uint8_t bestFREQ = currentFREQ;
+    uint16_t maxMean = freqEstimatedMeans[0];
+
+    for (uint8_t idx = 0; idx < NUM_FREQ; idx++) {
+      uint16_t sample = rand() % TS_SCALE; // Scaled random sample between 0 and TS_SCALE
+      if (freqEstimatedMeans[idx] > maxMean && sample > maxMean) {
+        maxMean = freqEstimatedMeans[idx];
+        bestFREQ = idx;
+      }
+
+      /*
+      Serial.print("TS sample for FREQ ");
+      Serial.print(idx);
+      Serial.print(" , ");
+      Serial.println(sample);
+      */
+    }
+
+    return bestFREQ;
+  }
+
+  void ThompsonSampling::freqUpdate(uint8_t currentFREQ, uint8_t reward)
+  {
+    uint8_t idx = idxToFreq(currentFREQ);
+
+    /*
+    Serial.print("TS reward for FREQ ");
+    Serial.print(idx);
+    Serial.print(" , ");
+    Serial.println(reward);
+    */
+
+    freqPulls[idx]++;
+    freqRewards[idx] += reward * TS_SCALE;
+    
+    if (freqPulls[idx] > 0) {
+      freqEstimatedMeans[idx] = freqRewards[idx] / freqPulls[idx];
+    }
+  }
+#endif
--- src/ThompsonSampling.h
+++ src/ThompsonSampling.h
@@ -3,8 +3,8 @@
 
 #include "Bandit.h"
 #include <stdlib.h>
+#include <Arduino.h>
 
-#define DEFAULT_SF 9
 #define TS_SCALE 1000
 
 class ThompsonSampling: private Bandit
@@ -14,13 +14,24 @@ class ThompsonSampling: private Bandit
     ThompsonSampling();
     ~ThompsonSampling();
 
-    uint8_t pull();
-    void update(uint8_t sf, uint8_t reward);
+    uint8_t pull(uint8_t currentSQ);
+    void update(uint8_t currentSQ, uint8_t reward);
+
+    #if FREQ_ENABLE
+      uint8_t freqPull(uint8_t currentFREQ);
+      void freqUpdate(uint8_t currentFREQ, uint8_t reward);
+    #endif
 
   private:
     volatile uint32_t rewards[NUM_SF];
     volatile uint32_t pulls[NUM_SF];
     volatile uint16_t estimatedMeans[NUM_SF];
+
+    #if FREQ_ENABLE
+      volatile uint32_t freqRewards[NUM_SF];
+      volatile uint32_t freqPulls[NUM_SF];
+      volatile uint16_t freqEstimatedMeans[NUM_SF];
+    #endif
 };
 
 #endif
